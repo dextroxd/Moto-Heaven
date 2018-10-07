ff2e86d0fef3f1be073234d16178b911fcde205d
dextroxd <divyanshu171299@gmail.com>
2018-10-07 19:00:30 +0530

    Added automatic scroll and resolved bugs

--- app/src/main/java/com/dextroxd/motoheaven/activities/EachCard.java
+++ app/src/main/java/com/dextroxd/motoheaven/activities/EachCard.java
@@ -1,8 +1,13 @@
 package com.dextroxd.motoheaven.activities;
 
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -10,17 +15,28 @@ import com.dextroxd.motoheaven.R;
 import com.dextroxd.motoheaven.adapters.ViewPagerAdapter;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static android.util.Log.i;
 
 public class EachCard extends AppCompatActivity {
 
     ViewPager viewPager;
     ArrayList<String> imagee;
-
+    int currentPage = 0;
+    Timer timer;
+    final long DELAY_MS = 200;//delay in milliseconds before task is to be executed
+    final long PERIOD_MS = 2000; // time in milliseconds between successive task executions.
+    LinearLayout sliderDotspanel;
+    private int dotscount;
+    private ImageView[] dots;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_each_card);
          viewPager= findViewById(R.id.viewPager);
+        sliderDotspanel = (LinearLayout) findViewById(R.id.SliderDots);
          Bundle bundle = getIntent().getExtras();
         TextView tv = findViewById(R.id.description);
         TextView tvh=findViewById(R.id.heading);
@@ -67,8 +83,75 @@ public class EachCard extends AppCompatActivity {
 
         }
 
-        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this,imagee);
+        final ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this,imagee);
         viewPager.setAdapter(viewPagerAdapter);
+        dotscount = viewPagerAdapter.getCount();
+        dots = new ImageView[dotscount];
 
+        for(int i = 0; i < dotscount; i++){
+
+            dots[i] = new ImageView(this);
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.nonactive_dot));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            params.setMargins(8, 0, 8, 0);
+
+            sliderDotspanel.addView(dots[i], params);
+
+        }
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                currentPage = viewPager.getCurrentItem();
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                for (int i = 0; i < dotscount; i++) {
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.nonactive_dot));
+                }
+
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+
+            final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == 5) {
+                    currentPage = 0;
+                }
+
+                viewPager.setCurrentItem(currentPage++, true);
+
+            }
+        };
+
+        timer = new Timer(); // This will create a new Thread
+        timer .schedule(new TimerTask() { // task to be scheduled
+
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, DELAY_MS, PERIOD_MS);
+    }
+
+    @Override
+    protected void onDestroy() {
+        timer.cancel();
+        super.onDestroy();
     }
 }
+
